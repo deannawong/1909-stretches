a78e43cf5a89bf1c349b24f298ed4ae367971724
Deanna Wong <dwong221@gmail.com>
2019-10-28 18:57:28 -0400

    first attempt solution to floydsTriangle

--- questions/12_floydsTriangle/floydsTriangle.js
+++ questions/12_floydsTriangle/floydsTriangle.js
@@ -6,6 +6,18 @@
 //  23
 //  456
 
-const floydsTriangle = n => {};
+const floydsTriangle = n => {
+    let result='';
+    
+    for(let i=1;i<n;i++){
+        let currentNum=1;
+        
+        for(let j=0;j<i;j++){
+            result+=currentNum;
+            ++currentNum;
+        }
+    }
+    return result;
+};
 
 module.exports = { floydsTriangle };

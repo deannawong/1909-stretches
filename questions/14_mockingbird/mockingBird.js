999fb95021f391f1d11bbbf8e3b3b2d3f4bf62ea
Deanna Wong <dwong221@gmail.com>
2019-11-11 18:50:26 -0500

    solution to mockingBird

--- questions/14_mockingbird/mockingBird.js
+++ questions/14_mockingbird/mockingBird.js
@@ -1,5 +1,11 @@
 const repeater = (func, n) => {
   // repeater runs a function n times and returns an array of its outputs
+
+  let returnArr = [];
+  for (let i = 0; i < n; i++) {
+    returnArr.push(func());
+  }
+  return returnArr;
 };
 
 module.exports = { repeater };

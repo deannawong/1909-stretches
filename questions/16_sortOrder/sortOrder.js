4ab189a46aecd59a03a41ddae5804da265ce799a
Deanna Wong <dwong221@gmail.com>
2019-11-18 19:00:24 -0500

    Only sorts when value of key to be sorted on is a number

--- questions/16_sortOrder/sortOrder.js
+++ questions/16_sortOrder/sortOrder.js
@@ -3,6 +3,22 @@ SortOrder takes an array of objects, a key to be sorted on
 and a key for the return value. 
 */
 
-function sortOrder() {}
+function sortOrder(arr, sortKey, returnKey) {
+  let result = [];
+  let sortVals = [];
+  arr.forEach(obj => {
+    sortVals.push(obj[sortKey]);
+  });
+
+  sortVals.sort().map((sortKeyVal, idx) => {
+    arr.forEach(obj => {
+      if (obj[sortKey] === sortKeyVal) {
+        result[idx] = obj[returnKey];
+      }
+    });
+  });
+
+  return result;
+}
 
 module.exports = { sortOrder };

8983f014b0b07ec9b9eb437d731af9ba0b7c1914
Deanna Wong <dwong221@gmail.com>
2019-11-20 18:43:21 -0500

    In class solution added

--- questions/16_sortOrder/sortOrder.js
+++ questions/16_sortOrder/sortOrder.js
@@ -4,21 +4,32 @@ and a key for the return value.
 */
 
 function sortOrder(arr, sortKey, returnKey) {
-  let result = [];
-  let sortVals = [];
-  arr.forEach(obj => {
-    sortVals.push(obj[sortKey]);
-  });
+  //   let result = [];
+  //   let sortVals = [];
+  //   arr.forEach(obj => {
+  //     sortVals.push(obj[sortKey]);
+  //   });
 
-  sortVals.sort().map((sortKeyVal, idx) => {
-    arr.forEach(obj => {
-      if (obj[sortKey] === sortKeyVal) {
-        result[idx] = obj[returnKey];
+  //   sortVals.sort().map((sortKeyVal, idx) => {
+  //     arr.forEach(obj => {
+  //       if (obj[sortKey] === sortKeyVal) {
+  //         result[idx] = obj[returnKey];
+  //       }
+  //     });
+  //   });
+
+  //   return result;
+  return arr
+    .sort((a, b) => {
+      if (a[sortKey] < b[sortKey]) {
+        return -1;
       }
-    });
-  });
-
-  return result;
+      if (a[sortKey] > b[sortKey]) {
+        return 1;
+      }
+      return 0;
+    })
+    .map(element => element[returnKey]);
 }
 
 module.exports = { sortOrder };

9a5e3d4f8a00dc63c6345112abb063bd9a1a4419
Deanna Wong <dwong221@gmail.com>
2019-12-04 18:55:56 -0500

    solution w/o extra credit

--- questions/19_calcluator/calculator.js
+++ questions/19_calcluator/calculator.js
@@ -1,3 +1,30 @@
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.value = 0;
+  }
+
+  add(num) {
+    this.value += num;
+    return this;
+  }
+  subtract(num) {
+    this.value -= num;
+    return this;
+  }
+  divide(num) {
+    if (num !== 0) {
+      this.value = this.value / num;
+      return this;
+    }
+  }
+  multiply(num) {
+    this.value = this.value * num;
+    return this;
+  }
+  clear() {
+    this.value = 0;
+    return this;
+  }
+}
 
 module.exports = { Calculator };
